6f490c0ca261d4dae4465021572e0849f16c9da6
Nyxze <manach.alexy@gmail.com>
2025-06-12 22:01:15 +0200

    feat: update Jest configuration to support ESM and TypeScript transformations

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 }; 
